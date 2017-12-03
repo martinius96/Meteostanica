cfb511496f614902d68dd77b79e577e676faab5b
Martin <martinius96@gmail.com>
2017-12-03 16:16:30 +0100

    Fix pre nahravanie dat

--- add.php
+++ add.php
@@ -1,14 +1,16 @@
 <?php
-header('Content-Type: text/html; charset=utf-8'); 
  include ("connect.php");
-	$temp1=$_GET["temp1"];
-  $temp2=$_GET["temp2"];
-  $pres1=$_GET["pres1"];
-  $hum1=$_GET["hum1"];
-  
-$ins = mysqli_query($con,"INSERT INTO `TempOutside` (`temperature`) VALUES ('".$_GET["temp1"]."')") or die (mysqli_error($con)); 
-$ins2 = mysqli_query($con,"INSERT INTO `TempLivingRoom` (`temperature`) VALUES ('".$_GET["temp2"]."')") or die (mysqli_error($con));
-$ins3 = mysqli_query($con,"INSERT INTO `PressureOutside` (`pressure`) VALUES ('".$_GET["pres1"]."')") or die (mysqli_error($con)); 
-$ins4 = mysqli_query($con,"INSERT INTO `Humidity` (`humidity`) VALUES ('".$_GET["hum1"]."')") or die (mysqli_error($con));  
-   
+ $temp1 = $_GET["temp1"];
+ $t2 = $_GET["tempinside"];
+ $p = $_GET["pressure"];
+ $h = $_GET["humidity"];
+ if($temp1== "" || $t2=="" || $p=="" || $h=="" || $temp1== 0 || $t2==0 || $p==0 || $h==0 || $temp1< -40 || $t2< -40 || $temp1>50 || $t2>50 || $p>1050 || $p<950 || $h==0.01 || $h==0.02 || $h==0.03 || $h==95){
+ 	echo 'Zabránenie nesprávnemu typu/hodnote dát uložiť sa do databázy!';
+ }
+ else{
+$ins = mysqli_query($con,"INSERT INTO `TempOutside` (`temperature`) VALUES ('".$temp1."')") or die (mysqli_error($con)); 
+$ins2 = mysqli_query($con,"INSERT INTO `TempLivingRoom` (`temperature`) VALUES ('".$t2."')") or die (mysqli_error($con));
+$ins3 = mysqli_query($con,"INSERT INTO `PressureOutside` (`pressure`) VALUES ('".$p."')") or die (mysqli_error($con)); 
+$ins4 = mysqli_query($con,"INSERT INTO `Humidity` (`humidity`) VALUES ('".$h."')") or die (mysqli_error($con));  
+   }
 ?>
