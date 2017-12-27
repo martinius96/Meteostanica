e043b5b76a73e90759a5464def4dc6896078d728
Martin <martinius96@gmail.com>
2017-12-27 22:08:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,17 +21,6 @@ najnižšia teplota, °C, stupne, tlak, hPa, atmosferický tlak, atmosféra, pro
 <link rel="stylesheet" href="css/style.css">
 <link rel="shortcut icon" type="image/x-icon" href="img/meteostation.ico">
 <link rel="sitemap" type="application/xml" title="Mapa stránky" href="sitemap.xml">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-76290977-2', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 <script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['gauge']}]}"></script>
 <?php include("connect.php"); ?>
  <?php
