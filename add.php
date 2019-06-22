eddea8be0ef1b1adf425a2b76ab6ef52d6a6b0c7
Martin <martinius96@gmail.com>
2019-06-23 01:22:35 +0200

    Update add.php

--- add.php
+++ add.php
@@ -1,9 +1,9 @@
 <?php
  include ("connect.php");
- $temp1 = $_GET["temp1"];
- $t2 = $_GET["tempinside"];
- $p = $_GET["pressure"];
- $h = $_GET["humidity"];
+$temp1 = mysqli_real_escape_string($con, $_GET['temp1']);
+$t2 = mysqli_real_escape_string($con, $_GET['tempinside']);
+$p = mysqli_real_escape_string($con, $_GET['pressure']);
+$h = mysqli_real_escape_string($con, $_GET['humidity']);
  if($temp1== "" || $t2=="" || $p=="" || $h=="" || $temp1== 0 || $t2==0 || $p==0 || $h==0 || $temp1< -40 || $t2< -40 || $temp1>50 || $t2>50 || $p>1050 || $p<950 || $h==0.01 || $h==0.02 || $h==0.03 || $h==95){
  	echo 'Zabránenie nesprávnemu typu/hodnote dát uložiť sa do databázy!';
  }
